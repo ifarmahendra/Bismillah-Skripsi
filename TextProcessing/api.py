5d426f18fc4f11668feb6f1e59b65367888f09db
ifarmahendra <55471201+ifarmahendra@users.noreply.github.com>
2022-07-01 18:49:55 +0700

    Update api.py

--- TextProcessing/api.py
+++ TextProcessing/api.py
@@ -211,4 +211,4 @@ def job():
         })
 
 if __name__ == '__main__':
-    app.run(port=8001, debug=True) #menjalankan server flask pada port 8001
+    app.run(host="0.0.0.0", port=8001, debug=True) #menjalankan server flask pada port 8001
