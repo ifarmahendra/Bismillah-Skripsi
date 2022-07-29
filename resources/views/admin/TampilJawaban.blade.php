d0355911b6ccab896304ebc1ed5128fc5f1221bc
ifarmahendra <55471201+ifarmahendra@users.noreply.github.com>
2022-07-29 16:22:18 +0700

    Update TampilJawaban.blade.php

--- resources/views/admin/TampilJawaban.blade.php
+++ resources/views/admin/TampilJawaban.blade.php
@@ -104,7 +104,11 @@
                                                 }
                                                 @endphp
                                                 @if ($nilai == 'null')
+                                                @if(round($new::where('formjawaban_id', $dt->id)->first()->nilai_cosine ?? 0,2)*100 == 0)
+                                                {{round($new::where('formjawaban_id', $dt->id)->first()->nilai_cosine ?? 0,2)*100}}
+                                                @else
                                                 Nilai Masih Diproses
+                                                @endif
                                                 @else
                                                 {{$nilai}}
                                                 @endif
