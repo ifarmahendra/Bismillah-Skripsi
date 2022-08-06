d9e0eee59de09992f8408ee1d81e338cf05f11b5
ifarmahendra <55471201+ifarmahendra@users.noreply.github.com>
2022-08-06 15:51:12 +0700

    Update forminput.blade.php

--- resources/views/forminput.blade.php
+++ resources/views/forminput.blade.php
@@ -54,12 +54,11 @@
                         <div class="form-group">
                             <label for="tanggal">Tanggal Perkuliahan</label>
                             <input type="datetime-local" class="form-input" name="tanggal" id="tanggal" required="required" />
-                        </div>l>
+                        </div>
                             @foreach($learning as $lr )
                             <input type="hidden" class="form-input" name="matkul_id"
                                 value="{{$lr->id}}"
                                 id="matkul_id" required="required" readonly>
-                            <textarea class="form-input" name="soal_id" id="soal_id" cols="75" rows="10" style="width:100%" required="required" readonly>{{$lr->id}}</textarea>
                             @endforeach
                         <div class="form-group form-group-textarea mb-md-0">
                             <label for="jawaban">Jawaban</label>
